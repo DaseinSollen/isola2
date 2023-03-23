e453921ce447d8f9f89d0ea2eebc8988950db283
dennis <dennispirotta@gmail.com>
2023-03-23 13:41:11 +0100

    Dump request in debugbar

--- app/Http/Controllers/ApiCameraController.php
+++ app/Http/Controllers/ApiCameraController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Events\NewCar;
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Event;
 use ProtoneMedia\Splade\Facades\Splade;
@@ -17,6 +18,7 @@ class ApiCameraController extends Controller
     public function store(Request $request)
     {
         event(new NewCar($request->get('value','XX999XX')));
+        Debugbar::info($request);
         return response()->json('Event Dispatched');
     }
 }
