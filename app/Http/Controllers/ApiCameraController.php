f3028e9ae43cf9f6288da1c2ecd0585549763ebd
dennis <dennispirotta@gmail.com>
2023-03-23 08:53:47 +0100

    Creato il progetto isola2 con Laravel Splade

---
+++ app/Http/Controllers/ApiCameraController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\NewCar;
+use Illuminate\Support\Facades\Event;
+use ProtoneMedia\Splade\Facades\Splade;
+
+class ApiCameraController extends Controller
+{
+    public function index()
+    {
+        event(new NewCar("00AAA00"));
+        return "Event Dispatched";
+    }
+}
