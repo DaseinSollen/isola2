ab43dfc14cb8da771133dbcab9674a887a132fb1
dennis <dennispirotta@gmail.com>
2023-03-24 11:06:22 +0100

    🔥 refactor(.gitignore): escludi il file vite.config.js 🔥 refactor(vite.config.js): rimuovi la variabile server e usa direttamente l'oggetto host ✨ funzionalità(vite.config.js): aggiungi supporto per la configurazione di vite.config.js.example

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,16 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import fs from 'fs';
 
-let server = {}
-if(import.meta.env.APP_DEBUG){
-    const host = 'isola2.dev'
-    server = {
+const host = 'isola2.dev'
+export default defineConfig({
+    server: {
         host,
-            hmr: { host },
+        hmr: { host },
         https: {
             key: fs.readFileSync('C:\\laragon\\etc\\ssl\\laragon.key'),
-                cert: fs.readFileSync('C:\\laragon\\etc\\ssl\\laragon.crt')
+            cert: fs.readFileSync('C:\\laragon\\etc\\ssl\\laragon.crt')
         }
-    }
-}
-
-
-export default defineConfig({
-    server: server,
+    },
     plugins: [
         laravel({
             input: "resources/js/app.js",
