5bcc02c9dcadf6d5068cceed878d08a53f5dda7b
dennis <dennispirotta@gmail.com>
2023-03-27 10:25:05 +0200

    ✨ funzionalità(OperatorController.php): aggiungi Controller per Operator ✨ funzionalità(Operator.php): aggiungi Modello per Operator
    🎉 feat(OperatorFactory.php): aggiungi factory per il modello Operator
    🎉 feat(create_operators_table.php): aggiungi migrazione per la creazione della tabella operators
    
    ✨ funzionalità(login.blade.php): aggiungi pagina di login per gli operatori con supporto per il recupero password e la funzionalità "Remember me"
    
    ✨ funzionalità(register.blade.php): aggiungi pagina di registrazione per gli operatori
    🎨 stile(register.blade.php): aggiungi stile alla pagina di registrazione per gli operatori

---
+++ app/Models/Operator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasTeams;
+use Laravel\Sanctum\HasApiTokens;
+
+class Operator extends Authenticatable
+{
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use HasTeams;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
+}
